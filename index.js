da7f1b6adaa8152c69949b0892bdba6eb3c3871d
thecoderishi <rishirverma512@gmail.com>
2024-02-11 17:55:40 +0530

    socket io implementation

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const { Server } = require('socket.io')
+const io = new Server(3000, { cors: { origin: ['http://localhost:5173'] } })
 
-app.get('/', (req, res) => {
-  res.status(200).json('tic-tac-toe home update')
-})
 
-app.listen(PORT, () => {
-  console.log(`The app is up and running at port ${PORT}`);
-})
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
 
-module.exports = app
+  socket.on('create-room', (roomId, cb) => {
+    socket.join(roomId)
+    cb(`Created a room id ${roomId}`)
+  })
+
+  socket.on('join-room', async (roomId, cb) => {
+    const sockets = await io.in(roomId).fetchSockets();
+    if (sockets.length === 0) {
+      cb(false, `No room found for room id ${roomId}`)
+    } else if (sockets.length > 1) {
+      cb(false, `You can not join room id ${roomId}`)
+    } else {
+      socket.join(roomId)
+      socket.to(roomId).emit('joined')
+      cb(true, `Joined room id ${roomId}`)
+    }
+  })
+
+  socket.on('update-game', (roomId, playedMove, index) => {
+    socket.to(roomId).emit('update-game', playedMove, index)
+  })
+
+  socket.on('won-game', (roomId, playedMove, index, winningLine) => {
+    socket.to(roomId).emit('won-game', playedMove, index, winningLine)
+  })
+
+  socket.on('reset', (roomId) => {
+    socket.to(roomId).emit('reset')
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log(`socket ${socket.id} disconnected due to ${reason}`);
+  });
+});
